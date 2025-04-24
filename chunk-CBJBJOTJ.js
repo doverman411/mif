e05782097528617e5699805cbc01e02cd2ff1d3e
David Overman <portalerd@icloud.com>
2025-04-23 22:07:36 -0700

    Auto-generated commit

--- chunk-CBJBJOTJ.js
+++
@@ -1,1 +0,0 @@
-import{Ca as n,Ma as o,Na as c,Wa as a}from"./chunk-XP65X3I6.js";var p=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-contact"]],decls:2,vars:0,template:function(t,s){t&1&&(o(0,"p"),a(1,"contact works!"),c())},encapsulation:2})};export{p as ContactComponent};
